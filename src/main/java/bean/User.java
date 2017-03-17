39497eb63be10247fbafcd31eacd9f6761eeff47
Dominik Enßle <dominik.enssle@web.de>
2017-03-17 16:09:06 +0100

    user: add id

--- src/main/java/bean/User.java
+++ src/main/java/bean/User.java
@@ -1,14 +1,20 @@
 package bean;
 
 public class User {
+	private int id;
     private String name;
     private String password;
-
-    public User(String name, String password) {
+    
+    public User(int id, String name, String password) {
+    	this.id = id; 
         this.name = name;
         this.password = password;
     }
 
+    public int getId() {
+    	return id;
+    }
+    
     public String getName() {
         return name;
     }
